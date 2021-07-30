04cdc6379f2fb4fe60a1a1203e5ba9276e1a82d8
Majid Fathi <majidfathi69@gmail.com>
2021-07-30 17:26:05 +0430

    Header.vue beautify code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,82 +1,42 @@
 <template>
     <div class="header" style="height: 136px; width: 1170px; background-color: #FFFFFF;">
-        <div style="height: 47px; width: 1110px; background-color: #FFFFFF; margin-left: 30px; margin-right: 30px; display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    align-content: center;
-    justify-content: flex-start;
-    align-items: center;justify-content: space-between;">
-            <div style="height: 47px; width: 316px; background-color: #FFFFFF; display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    align-content: flex-start;
-    justify-content: flex-start;
-    align-items: center;">
-                <img src="../assets/logo.png" alt="">
-    <div style="width:15px;"></div>
-                <div style="font-size: 32px;
-    text-align: left;
-    word-spacing: 44px;
-    color: #131313;">Website</div>
-            </div>
-    <div class="SizedBox" style="width: 15px;"></div>
-                <img src="../assets/more.png" alt="" style="height: 30px;
-    width: 30px;">
-            <div style="display: flex;
-    flex-direction: row;
-    align-items: center;">
-                <img src="../assets/avatar1.png" alt="" style="height: 30px;
-    width: 30px;">
-                <img src="../assets/avatar2.png" alt="" style="height: 30px;
-    width: 30px;">
-                <img src="../assets/avatar3.png" alt="" style="height: 30px;
-    width: 30px;">
-    <div  class="SizedBox" style="width: 20px;"></div>
-    <button style="height: 30px; width: 67px;
-    background-color: #EAEAEA;    border-radius: 15px;border-width: 0;"><span style="font-size: 14px;
-    color: #131313;font-weight:bold;    ">Share</span></button>
-    <div  class="SizedBox" style="width: 15px;"></div>
-    <button style=" display: flex; width: 81px; height: 30px;align-content: center;
-    justify-content: center;
-    align-items: center;
-    background-color: #FFF8DD;border-radius: 15px;    border-width: 0;"><img src="../assets/messages.png" alt="" style="height: 16px;
-    width: 16px;"><div style="width: 5px;"></div>
-    <span style="font-size: 14px;
-    color: #FFC200;font-weight:bold;">Chat</span></button>
+        <div style="height: 47px; width: 1110px; background-color: #FFFFFF; margin-left: 30px; margin-right: 30px; display: flex; flex-direction: row; flex-wrap: nowrap; align-content: center; justify-content: flex-start; align-items: center;justify-content: space-between;">
+                <div style="display: flex; flex-direction: row; justify-content: flex-start; align-items: center;">
+                     <div style="height: 47px; background-color: #FFFFFF; display: flex; flex-direction: row; flex-wrap: nowrap; align-content: flex-start; justify-content: flex-start; align-items: center;">
+                        <img src="../assets/logo.png" alt="">
+                        <div style="width:15px;"></div>
+                        <div style="font-size: 32px; text-align: left; word-spacing: 44px; color: #131313;">Website</div>
+                        </div>
+                        <div class="SizedBox" style="width: 15px;"></div>
+                        <img src="../assets/more.png" alt="" style="height: 30px; width: 30px;">   
+                </div>
                 
-    
-    </div>
+                <div style="display: flex; flex-direction: row; align-items: center;">
+                        <img src="../assets/avatar1.png" alt="" style="height: 30px; width: 30px;">
+                        <img src="../assets/avatar2.png" alt="" style="height: 30px; width: 30px;">
+                        <img src="../assets/avatar3.png" alt="" style="height: 30px; width: 30px;">
+                        <div  class="SizedBox" style="width: 20px;"></div>
+                        <button style="height: 30px; width: 67px; background-color: #EAEAEA;    border-radius: 15px;border-width: 0;"><span style="font-size: 14px; color: #131313;font-weight:bold;">Share</span></button>
+                        <div  class="SizedBox" style="width: 15px;"></div>
+                        <button style=" display: flex; width: 81px; height: 30px;align-content: center; justify-content: center; align-items: center; background-color: #FFF8DD;border-radius: 15px;    border-width: 0;"><img src="../assets/messages.png" alt="" style="height: 16px; width: 16px;"><div style="width: 5px;"></div>
+                        <span style="font-size: 14px; color: #FFC200;font-weight:bold;">Chat</span></button>
+                </div>
         </div>
         <div style="height: 20px;"></div>
-        <div style="
-    display: flex;
-    flex-direction: row;
-    align-content: flex-end;
-    justify-content: flex-start;
-    align-items: flex-start;
-    margin-left: 30px;
-    margin-right: 30px;
-">
-
-            <span style="font-size: 14px;
-    color: rgba(19, 19, 19, 0.7);font-weight:bold;    ">Tasks</span>
-    <div style="width: 33px;"></div>
-            <span style="font-size: 14px;
-    color: rgba(19, 19, 19, 0.7);font-weight:bold;    ">Kanban</span>
-    <div style="width: 28px;"></div>
-            <span style="font-size: 14px;
-    color: rgba(19, 19, 19, 0.7);font-weight:bold;    ">Activity</span>
-    <div style="width: 27px;"></div>
-            <span style="font-size: 14px;
-    color: rgba(19, 19, 19, 0.7);font-weight:bold;    ">Calendar</span>
-    <div style="width: 29px;"></div>
-    <div><span style="font-size: 14px;
-    color: rgb(19, 19, 19);font-weight:bold;    ">Files</span>
-    <div style="height: 11px;"></div>
-    <div style="height: 2px; background-color: rgb(255,194,0);">
-
-    </div></div>
-            
+        <div style="display: flex; flex-direction: row; align-content: flex-end; justify-content: flex-start; align-items: flex-start; margin-left: 30px; margin-right: 30px;">
+                <span style="font-size: 14px; color: rgba(19, 19, 19, 0.7);font-weight:bold;">Tasks</span>
+                <div style="width: 33px;"></div>
+                <span style="font-size: 14px; color: rgba(19, 19, 19, 0.7);font-weight:bold;">Kanban</span>
+                <div style="width: 28px;"></div>
+                <span style="font-size: 14px; color: rgba(19, 19, 19, 0.7);font-weight:bold;">Activity</span>
+                <div style="width: 27px;"></div>
+                <span style="font-size: 14px; color: rgba(19, 19, 19, 0.7);font-weight:bold;">Calendar</span>
+                <div style="width: 29px;"></div>
+                <div>
+                        <span style="font-size: 14px; color: rgb(19, 19, 19);font-weight:bold;">Files</span>
+                        <div style="height: 11px;"></div>
+                        <div style="height: 2px; background-color: rgb(255,194,0);"></div>
+                </div>
         </div>
     </div>
 </template>
